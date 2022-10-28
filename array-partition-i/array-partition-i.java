29d5e5e61766e729c5b4852f8d061e2cf3d36df9
tarun-balwani <91433491+tarun-balwani@users.noreply.github.com>
2022-10-28 19:54:39 -0400

    Time: 24 ms, Memory: 53.5 MB - LeetHub

---
+++ array-partition-i/array-partition-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int temporary;
+        int maxSum = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if (nums[i] > nums[j]) {
+        //             temporary = nums[i];
+        //             nums[i] = nums[j];
+        //             nums[j] = temporary;
+        //         }
+        //     }
+        // }
+        Arrays.sort(nums);
+        
+       for(int i = 0; i < nums.length -1; i+=2){
+           maxSum += nums[i];
+       }
+        return maxSum;
+    }
+}

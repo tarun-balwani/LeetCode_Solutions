35684dc6c175aedc590df967b0da02f61c4fec02
tarun-balwani <91433491+tarun-balwani@users.noreply.github.com>
2023-05-13 09:33:05 -0400

    Time: 0 ms, Memory: 58.7 MB - LeetHub

--- rotate-array/rotate-array.java
+++ rotate-array/rotate-array.java
@@ -1,45 +1,22 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-             int totalRotations = k % nums.length;
-        
-        int i = 0;
-        int j = nums.length-1;
-        int temp = 0;
-        
-        //reversing the i/p array
-        while(i < j){
-            temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            j--;
-            i++;
-        }
-        
-        i = totalRotations;
-        j = nums.length-1;
-        while(i < j){
-            temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            j--;
-            i++;
-        }
-        
-        i = 0;
-        j = totalRotations-1;
-        while(i < j){
-            temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            j--;
-            i++;
-        }
-        
-        for(int a: nums){
-            System.out.println(a);
-        }
+        k = k%nums.length;
+        //reverse the entire array
+        reverse(nums, 0, nums.length-1);
+        //reversing the first k elements in the reversed array
+        reverse(nums, 0,k-1);
+        //reverse the elements after K to the end
+        reverse(nums,k,nums.length-1);
+    }
     
-        
-       
+    public void reverse(int[] nums,int left, int right){
+        while(left < right){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+            
+        }
     }
 }

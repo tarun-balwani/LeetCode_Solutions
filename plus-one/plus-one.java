6d0fc8beb041176b0970d08dec62481ed44459ee
tarun-balwani <91433491+tarun-balwani@users.noreply.github.com>
2023-05-14 08:20:33 -0400

    Time: 0 ms, Memory: 41.2 MB - LeetHub

--- plus-one/plus-one.java
+++ plus-one/plus-one.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        
         for(int i = digits.length-1; i >= 0; i--){
             if(digits[i] < 9){
                 digits[i]++;
@@ -8,8 +9,8 @@ class Solution {
             digits[i] = 0;
         }
         
-        digits = new int[digits.length+1];
-        digits[0] = 1;
-        return digits;
-}
+        int[] ans = new int[digits.length+1];
+        ans[0] = 1;
+        return ans;
+    }
 }

e74a28988c1c537f69ab5853ed44431fca0b0b83
tarun-balwani <91433491+tarun-balwani@users.noreply.github.com>
2023-05-14 08:29:41 -0400

    Time: 1 ms, Memory: 44 MB - LeetHub

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -1,17 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+     int[] ans = new int[nums.length];
+     int pointer = 0;
         
-        int[] movedZeroes = new int[nums.length];
-        int j = 0;
-        for(int num : nums){
-            if (num != 0){
-                movedZeroes[j] = num;
-                j++;
-            }
-        }
-        
+       for(int n : nums){
+           if(n != 0){
+               ans[pointer] = n;
+               pointer++;
+           }
+       }
+    
         for(int i = 0; i < nums.length; i++){
-            nums[i] = movedZeroes[i];
+            nums[i] = 0;
+        }
+        for(int i = 0; i < ans.length; i++){
+            nums[i] = ans[i];
         }
     }
 }

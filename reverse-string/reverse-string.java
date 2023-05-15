d3b6ce7d7fc8bc7508c2c0d8168dc206a777468c
tarun-balwani <91433491+tarun-balwani@users.noreply.github.com>
2023-05-15 08:13:24 -0400

    Time: 1 ms, Memory: 49.6 MB - LeetHub

--- reverse-string/reverse-string.java
+++ reverse-string/reverse-string.java
@@ -1,27 +1,11 @@
 class Solution {
     public void reverseString(char[] s) {
-// Iterative approach
-//          int startIndex = 0;
-//         int endIndex = s.length-1;
         
-//         while(startIndex < endIndex){
-//             char temp = s[endIndex];
-//             s[endIndex] = s[startIndex];
-//             s[startIndex] = temp;
-//             startIndex++;
-//             endIndex--;
-//         }
-        
-        // recursive approach
-        reversedString(s, 0, s.length-1);
-    }
-    
-    public void reversedString(char[] s, int left, int right){
-        if(left >= right) return;
-        
-            char temp = s[left];
-            s[left++] = s[right];
-            s[right--] = temp;
-            reversedString(s, left,right);        
+        for(int i = 0; i < s.length/2; i++){
+            char temp = s[i];
+            s[i] = s[s.length-1-i];
+            s[s.length-1-i] = temp;
+            
+        }
     }
 }
